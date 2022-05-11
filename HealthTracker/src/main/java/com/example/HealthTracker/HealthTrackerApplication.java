e02c10733bbfd4ae8a56db8a19e0ee229f6627a1
Eda Kaş <eda.kas60@gmail.com>
2022-05-11 13:32:00 +0300

    practice

--- HealthTracker/src/main/java/com/example/HealthTracker/HealthTrackerApplication.java
+++ HealthTracker/src/main/java/com/example/HealthTracker/HealthTrackerApplication.java
@@ -3,14 +3,15 @@ package com.example.HealthTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class HealthTrackerApplication {
+public  class HealthTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthTrackerApplication.class, args);
 		System.out.println("Hello");
 	}
-
 
 
 }
